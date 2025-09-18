39d369818b63f2566cc253d8c26887a49b1c1bc2
Phonaxy <romaindestruhaut@yahoo.fr>
2025-09-18 10:01:07 +0200

    ajout ressource

---
+++ ressource.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Phona on 18/09/2025.
+//
+
+#include "ressource.h"
---
+++ ressource.h
@@ -0,0 +1,14 @@
+//
+// Created by Phona on 18/09/2025.
+//
+
+#ifndef PROJETPOO_RESSOURCE_H
+#define PROJETPOO_RESSOURCE_H
+
+
+class ressource {
+
+};
+
+
+#endif //PROJETPOO_RESSOURCE_H
