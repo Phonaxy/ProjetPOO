56ef79346dd7a99a7d2a7ddf82c38a50cd80b086
Phonaxy <romaindestruhaut@yahoo.fr>
2025-09-25 11:26:04 +0200

    fix + update Ressource

--- include/Ressource.h
+++ include/Ressource.h
@@ -2,18 +2,25 @@
 #define PROJETPOO_RESSOURCE_H
 
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <sstream>
+#include <algorithm>
+#include <limits>
 
 class Ressource {
 protected:
-    int id;
+    int id{};
     std::string titre;
     std::string auteur;
-    bool dispo;
+    bool dispo{};
 
 public:
     Ressource(int id, const std::string& titre, const std::string& auteur, bool dispo = true);
     virtual ~Ressource() = default;
-
 
     virtual void afficher() const = 0;
     virtual void reserver();
@@ -24,6 +31,9 @@ public:
     const std::string& getTitre() const;
     const std::string& getAuteur() const;
     bool estDisponible() const;
+
+    // Pour la sauvegarde
+    virtual std::string toFileString() const = 0;
 };
 
 #endif
