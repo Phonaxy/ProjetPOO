54158a3f423721043415a08b433d9aa36c52648e
guyomard <gildasguyomard03@gmail.com>
2025-09-25 12:22:22 +0200

    fix revue + fonction

--- include/Revue.h
+++ include/Revue.h
@@ -13,14 +13,16 @@ private:
     int annee;
 
 public:
-    Revue(int id, const std::string& titre, const std::string& auteur,
+    Revue(int id, const std::string& titre, const std::string& auteur, bool dispo,
           int nbPages, const std::string& editeur, int nbArticles,
           const std::string& resume, int annee);
 
     void ajouterArticle(const std::string& article);
     void listerArticles() const;
-
     void afficher() const override;
+    std::string toFileString() const override;
+
+    void setArticlesFromString(const std::string& articlesString);
 };
 
 #endif
--- src/Revue.cpp
+++ src/Revue.cpp
@@ -1,14 +1,54 @@
 #include "../include/Revue.h"
 
 
-void Revue::ajouterArticle(const std::string &article) {
+Revue::Revue(int id, const std::string& titre, const std::string& auteur, bool dispo,
+             int nbPages, const std::string& editeur, int nbArticles,
+             const std::string& resume, int annee)
+        : Ressource(id, titre, auteur, dispo), nbPages(nbPages), editeur(editeur),
+          nbArticles(nbArticles), resume(resume), annee(annee) {}
 
+void Revue::ajouterArticle(const std::string& article) {
+    lesArticles.push_back(article);
 }
 
 void Revue::listerArticles() const {
-
+    std::cout << "Articles:\n";
+    for (const auto& article : lesArticles) {
+        std::cout << "- " << article << "\n";
+    }
 }
 
 void Revue::afficher() const {
+    std::cout << "=== REVUE ===\n";
+    std::cout << "ID: " << id << "\n";
+    std::cout << "Titre: " << titre << "\n";
+    std::cout << "Auteur: " << auteur << "\n";
+    std::cout << "Disponible: " << (dispo ? "Oui" : "Non") << "\n";
+    std::cout << "Nombre de pages: " << nbPages << "\n";
+    std::cout << "Editeur: " << editeur << "\n";
+    std::cout << "Nombre d'articles: " << nbArticles << "\n";
+    listerArticles();
+    std::cout << "Resume: " << resume << "\n";
+    std::cout << "Annee: " << annee << "\n";
+}
 
+std::string Revue::toFileString() const {
+    std::string articlesStr;
+    for (size_t i = 0; i < lesArticles.size(); ++i) {
+        if (i > 0) articlesStr += "|";
+        articlesStr += lesArticles[i];
+    }
+    return std::to_string(id) + ";Revue;" + titre + ";" + auteur + ";" +
+           (dispo ? "1" : "0") + ";" + std::to_string(nbPages) + ";" +
+           editeur + ";" + std::to_string(nbArticles) + ";" + articlesStr + ";" +
+           resume + ";" + std::to_string(annee);
+}
+
+void Revue::setArticlesFromString(const std::string& articlesString) {
+    lesArticles.clear();
+    std::stringstream ss(articlesString);
+    std::string article;
+    while (std::getline(ss, article, '|')) {
+        lesArticles.push_back(article);
+    }
 }
