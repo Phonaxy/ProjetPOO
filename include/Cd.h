a650c20cee8b30070d998050c9b3e10ee69f2468
guyomard <gildasguyomard03@gmail.com>
2025-09-25 11:13:55 +0200

    fix CD + fonction

--- include/Cd.h
+++ include/Cd.h
@@ -7,14 +7,14 @@ class CD : public Ressource {
 private:
     std::string maisonProd;
     int nbPistes;
-    double duree;
+    std::string duree;
 
 public:
-    CD(int id, const std::string& titre, const std::string& auteur,
-       const std::string& maisonProd, int nbPistes, double duree,bool dispo);
+    CD(int id, const std::string& titre, const std::string& auteur, bool dispo,
+       const std::string& maisonProd, int nbPistes, const std::string& duree);
 
     void afficher() const override;
+    std::string toFileString() const override;
 };
 
 #endif
-
